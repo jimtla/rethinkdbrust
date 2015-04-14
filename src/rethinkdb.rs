f0e513fb10b155ca46fc099d1c6f2601f07e4613
abraao.isvi <abraao.isvi@gmail.com>
2015-04-14 12:45:27 -0300

    more changes

--- src/rethinkdb.rs
+++ src/rethinkdb.rs
@@ -1,68 +1,65 @@
-mod rethinkdb {
 
-    pub struct Field(&str,&str)
+pub struct Field(&str,&str)
 
-    pub struct Element {
-        fields: Vec<Field>
+pub struct Element {
+    fields: Vec<Field>
+}
+
+pub struct Connection {
+    url: str,
+    port: str
+}
+
+pub struct Db {
+    name: str
+}
+
+pub struct Query {
+    stmt: str
+}
+
+pub struct Result {
+    pub status: i32,
+    pub message: String,
+    pub data: Vec<(String, String)>
+}
+
+impl Element {
+    fn add_field(&self, field: Field) {
+
+    }
+}
+
+impl DB {
+    fn table_create(name: &str)-> Query {
+
+    }
+}
+
+impl Table {
+    fn insert(elements: &Vec<Element>)-> Query {
+
+    }
+}
+
+impl Query {
+    pub fn run<F>(&self, Connection &con, callback: F)
+    where F : Fn(Result){
+
+    }
+}
+
+impl Connection {
+
+    pub fn connect()->Connection {
+
     }
 
-    pub struct Connection {
-        url: str,
-        port: str
+    pub fn use(&self, table: &str)-> Connection {
+
     }
 
-    pub struct Db {
-        name: str
+    pub fn db(&self, name: str)-> DB {
+
     }
-
-    pub struct Query {
-        stmt: str
-    }
-
-    pub struct Result {
-        pub status: i32,
-        pub message: String,
-        pub data: Vec<(String, String)>
-    }
-
-    impl Element {
-        fn add_field(&self, field: Field) {
-
-        }
-    }
-
-    impl DB {
-        fn table_create(name: &str)-> Query {
-
-        }
-    }
-
-    impl Table {
-        fn insert(elements: &Vec<Element>)-> Query {
-
-        }
-    }
-
-    impl Query {
-        pub fn run<F>(&self, Connection &con, callback: F)
-        where F : Fn(Result){
-
-        }
-    }
-
-    impl Connection {
-
-        pub fn connect()->Connection {
-
-        }
-
-        pub fn use(&self, table: &str)-> Connection {
-
-        }
-
-        pub fn db(&self, name: str)-> DB {
-
-        }
-    }
-
 }
