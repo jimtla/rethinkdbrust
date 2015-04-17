086e2f2f0f83b00958cb10592c026608e3b5378b
Paulo Suzart <paulosuzart@gmail.com>
2015-04-17 20:13:32 -0300

    Moved to pure json

--- src/rethinkdb.rs
+++ src/rethinkdb.rs
@@ -7,10 +7,11 @@ use byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
 use std::rc::Rc;
 use ql2::*;
 use rustc_serialize::json;
-use rustc_serialize::json::{ToJson, Json};
+use rustc_serialize::json::Json;
 use std::num::ToPrimitive;
 use std::string::String;
 use std::collections::BTreeMap;
+
 
 
 /* Structs to manage databse */
@@ -72,123 +73,47 @@ trait RQLQuery<'a> {
 
         true
     }
-    fn to_query_types(&'a self) -> Term;
+    fn to_query_types(&'a self) -> json::Json;
 
 }
 
 
-macro_rules! datum { //TODO: reduce repetition
-    (NUM => $value:expr) => {{
-            let mut datum = Datum::new();
-            datum.set_field_type(Datum_DatumType::R_NUM);
-            datum.set_r_num($value);
-            datum.compute_size();
-            datum
-    }};
-
-    (STR => $value:expr) => {{
-            let mut datum = Datum::new();
-            datum.set_field_type(Datum_DatumType::R_STR);
-            datum.set_r_str($value.clone());
-            datum.compute_size();
-            datum
-    }};
-
-}
-
-
-macro_rules! term_query {
-    ($ty:path, $args:expr, $opts:expr) => {{
-        let mut term = Term::new();
-        term.set_field_type($ty);
-        term.set_args(::protobuf::RepeatedField::from_vec($args));
-        term.set_optargs(::protobuf::RepeatedField::from_vec($opts));
-        term.compute_size();
-        term
-    }};
-    ($ty:path, $args:expr) => {{
-        let mut term = Term::new();
-        term.set_field_type($ty);
-        term.set_args(::protobuf::RepeatedField::from_vec($args));
-        term.compute_size();
-        term
-    }};
-
-}
-
-#[macro_export]
-macro_rules! term_datum {
-    ($datum:expr) => {{
-        let mut datum_term = Term::new();
-        datum_term.set_field_type(Term_TermType::DATUM);
-        datum_term.set_datum($datum);
-        datum_term.compute_size();
-        datum_term
-    }};
-    (STR => $value:expr) => {{
-       
-        let mut datum = datum!(STR => $value);
-        term_datum!(datum)
-
-    }};
-
-    (NUM => $value:expr) => {{
-       
-        let mut datum = datum!(NUM => $value); // uses forwarded type
-        term_datum!(datum)
-
-
-    }};
-
-
-}
-
-macro_rules! term_assoc_pair {
-    ($key:expr, $val:expr) => {{
-        let mut pair = Term_AssocPair::new();
-        pair.set_key($key.to_string());
-        pair.set_val($val);
-        pair.compute_size();
-        pair
-
-    }};
-}
 impl<'a> RQLQuery<'a> for TableCreate<'a> {
-    fn to_query_types(&'a self) -> Term {
+    fn to_query_types(&'a self) -> json::Json {
     
-    //    let table_crate_datum = str_datum!(self.name);
+        let mut j = Vec::new();
+        j.push(Json::I64(self.term as i64));
+        
+        let mut jd = Vec::new();
+        jd.push(self.db.to_query_types());
+        jd.push(Json::String(self.name.clone()));
 
-    // let mut table_crate_datum = Datum::new();
-    // table_crate_datum.set_field_type(Datum_DatumType::R_STR);
-    // table_crate_datum.set_r_str(self.name.clone());
-    // table_crate_datum.compute_size();
+        let mut d = BTreeMap::new();
+        d.insert("primary_key".to_string(), Json::String("id".to_string()));
+        d.insert("shards".to_string(), Json::I64(1 as i64));
+        d.insert("replicas".to_string(), Json::I64(1 as i64));
+        j.push(Json::Array(jd));
+        j.push(Json::Object(d));
+        Json::Array(j)
 
-    let mut primary_key_term = term_datum!(STR => "id".to_string());
-    let mut shars_term = term_datum!(NUM => 1f64);
-    let mut reps_term = term_datum!(NUM => 1f64);
-    let mut prims_term = term_datum!(STR => "X".to_string());
-
-    let mut opts_a = term_assoc_pair!("primary_key", primary_key_term);
-    let mut opts_b = term_assoc_pair!("shards", shars_term);
-    let mut opts_c = term_assoc_pair!("replicas", reps_term);
-    let mut opts_d = term_assoc_pair!("primary_replica_tag", prims_term);
-  
-
-    let table_create_datum_term = term_datum!(STR => self.name);
-
-    let mut table_create_term = term_query!(Term_TermType::TABLE_CREATE, 
-                                            vec![self.db.to_query_types(), table_create_datum_term],
-                                            vec![opts_a, opts_b, opts_c]);
-    
-    table_create_term
     }
 }
 
 
 impl<'a> RQLQuery<'a> for Db {
-    fn to_query_types(&'a self) -> Term {
-        let mut db_datum_term = term_datum!(STR => self.name);
-        term_query!(Term_TermType::DB, vec![db_datum_term])
+    fn to_query_types(&'a self) -> json::Json {
+        // [1,[39,[[15,[[14,["blog"]],"users"]],{"name":"Michel"}]],{}]
+        // [1,[60,[[14,["test"]],"person",{"primary_key":"id","replicas":1,"shards":1}]]]
+        
+        let mut j = Vec::new();
+        j.push(Json::I64(self.term as i64));
+        
+        let mut jd = Vec::new();
+        jd.push(Json::String(self.name.clone()));
+        j.push(Json::Array(jd));
+        
+        Json::Array(j)
+        
     }
 }
 
@@ -227,7 +152,7 @@ impl Connection {
     fn handshake(&mut self)  {
         self.stream.write_u32::<LittleEndian>(VersionDummy_Version::V0_4 as u32);
         self.stream.write_u32::<LittleEndian>(0);
-        self.stream.write_u32::<LittleEndian>(VersionDummy_Protocol::PROTOBUF as u32);
+        self.stream.write_u32::<LittleEndian>(VersionDummy_Protocol::JSON as u32);
         self.stream.flush();
         
         let mut res = Response::new();
@@ -259,24 +184,15 @@ fn test_connect() {
     // assert_eq!("db", db.stm);
     // //let qd = db.table_create("person").to_query_types();
     let tc = db.table_create("person"); //.run(&mut conn);
-
- 
-    let mut query = Query::new();
-    query.set_field_type(Query_QueryType::START);
-    query.set_token(2i64);
-    query.set_query(tc.to_query_types());
-    query.set_accepts_r_json(false);
-    println!("Tamanho query: {}", query.compute_size());
-
-
-    {
     
-        let mut writer = ::protobuf::stream::CodedOutputStream::new(&mut conn.stream);
+    conn.stream.write_i64::<LittleEndian>(1i64);
+    let json_query = tc.to_query_types();
+    let message = Json::Array(vec![Json::I64(1), json_query]).to_string();
+    let len = message.len();
+    conn.stream.write_i32::<LittleEndian>(len as i32);
+    println!("{}",message);
+    write!(conn.stream, "{}", message);
 
-        query.write_to_with_cached_sizes(&mut writer);
-        writer.flush();
-    }
-    
 
     let mut res = Response::new();
     let mut reader = ::protobuf::stream::CodedInputStream::new(&mut conn.stream);
